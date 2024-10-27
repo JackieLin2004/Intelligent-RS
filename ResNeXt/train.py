25d275875d2a5b554f35770e28be633e745e6cbd
JackieLin2004 <linxy20040304@163.com>
2024-10-27 15:45:02 +0800

    Completion of the basic architecture of ResNeXt

---
+++ ResNeXt/train.py
@@ -0,0 +1,133 @@
+import os
+import argparse
+
+import torch
+import torch.optim as optim
+from torchvision import transforms
+
+from my_dataset import MyDataSet
+from resnext_model import resnext101_32x8d as resnext101
+from utils import read_split_data, train_loop, get_logger
+
+
+def run(args):
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+
+    train_images_path, train_images_label, val_images_path, val_images_label = read_split_data(args.data_path)
+
+    img_size = 256
+    data_transform = {
+        "train": transforms.Compose([
+            transforms.Resize(int(img_size * 1.143)),
+            transforms.CenterCrop(img_size),
+            transforms.RandomHorizontalFlip(),  # 随机水平翻转
+            transforms.RandomRotation(90),  # 90度倍数随机旋转
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # 颜色抖动
+            transforms.RandomAffine(degrees=0, scale=(0.9, 1.1)),  # 仿射变换
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 标准化
+        ]),
+        "val": transforms.Compose([
+            transforms.Resize(int(img_size * 1.143)),
+            transforms.CenterCrop(img_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+    }
+
+    # 实例化训练数据集
+    train_dataset = MyDataSet(images_path=train_images_path,
+                              images_class=train_images_label,
+                              transform=data_transform["train"])
+
+    # 实例化验证数据集
+    val_dataset = MyDataSet(images_path=val_images_path,
+                            images_class=val_images_label,
+                            transform=data_transform["val"])
+
+    batch_size = args.batch_size
+    num_workers = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
+    print('Using {} dataloader workers every process'.format(num_workers))
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,
+                                               pin_memory=True,
+                                               num_workers=num_workers,
+                                               collate_fn=train_dataset.collate_fn)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=batch_size,
+                                             shuffle=False,
+                                             pin_memory=True,
+                                             num_workers=num_workers,
+                                             collate_fn=val_dataset.collate_fn)
+
+    model = resnext101(num_classes=args.num_classes).to(device)
+
+    if args.weights != "":
+        assert os.path.exists(args.weights), "weights file: '{}' not exist.".format(args.weights)
+        weights_dict = torch.load(args.weights, map_location=device)["model"]
+        # 删除有关分类类别的权重
+        for k in list(weights_dict.keys()):
+            if "head" in k:
+                del weights_dict[k]
+        print(model.load_state_dict(weights_dict, strict=False))
+
+    if args.freeze_layers:
+        for name, para in model.named_parameters():
+            # 除head外，其他权重全部冻结
+            if "head" not in name:
+                para.requires_grad_(False)
+            else:
+                print("training {}".format(name))
+
+    # 设置优化器
+    parameter_group = [p for p in model.parameters() if p.requires_grad]
+    optimizer = optim.AdamW(parameter_group, lr=args.lr, weight_decay=5E-2)
+
+    best_acc = 0.0
+    logger = get_logger()
+    logger.info("Start training and validating...")
+    for epoch in range(args.epochs):
+        val_acc, all_labels, all_preds = train_loop(
+            model=model,
+            optimizer=optimizer,
+            train_loader=train_loader,
+            val_loader=val_loader,
+            device=device,
+            epoch=epoch,
+            logger=logger,
+        )
+
+        if val_acc > best_acc:
+            best_acc = val_acc
+            torch.save(model.state_dict(), "best_model.pth")
+            logger.info("Best model saved!")
+
+            # 保存 all_labels 和 all_preds 到 txt 文件
+            with open("best_model_labels_preds.txt", "w") as f:
+                for label, pred in zip(all_labels, all_preds):
+                    f.write(f"{label} {pred}\n")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_classes', type=int, default=45)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--batch-size', type=int, default=8)
+    parser.add_argument('--lr', type=float, default=0.0001)
+
+    # 数据集所在根目录
+    parser.add_argument('--data-path', type=str,
+                        default="../dataset/classification")
+
+    # 预训练权重路径，如果不想载入就设置为空字符
+    parser.add_argument('--weights', type=str, default='',
+                        help='initial weights path')
+    # 是否冻结权重
+    parser.add_argument('--freeze-layers', type=bool, default=False)
+    parser.add_argument('--device', default='cuda:0', help='device id (i.e. 0 or 0,1 or cpu)')
+
+    options = parser.parse_args()
+
+    run(options)
